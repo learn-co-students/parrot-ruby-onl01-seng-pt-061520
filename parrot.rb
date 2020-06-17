a0efd53ea08fca5e165d64a8bef2f62f2fb2c7d0
Jessie Donovan <donovanajessie@gmail.com>
2020-06-17 02:11:46 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,5 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(string = "Squawk!")# Create method `parrot` that outputs a given phrase and
 # returns the phrase
+puts string
+string
+end
